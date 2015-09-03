c5d92b8c70f56616a7173f7663aeece5f5e73431
drome028 <romerodny@gmail.com>
2015-09-03 15:46:30 -0400

    Initial commits of send and receive scripts
    #517 - Initial commit, subject to change

---
+++ Code/subpub/rabbitmqTutorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api.js');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    ch.sendToQueue(q, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
