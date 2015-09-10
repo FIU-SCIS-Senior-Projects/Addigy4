29e4221bfc565977d52d666b800752d687482c8a
romerodny <romerodny@gmail.com>
2015-09-10 02:15:39 -0400

    New scripts open a terminal and echo hello world

---
+++ Code/subpub/client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import pika
+import uuid
+__author__ = 'David'
+
+
+class TerminalOpenClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to=self.callback_queue,
+                                         correlation_id=self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return str(self.response)
+
+terminal_rpc = TerminalOpenClient()
+
+print("[x] Requesting terminal open of 'Hello World!'")
+response = terminal_rpc.call("Hello World!")
+print("[.] Got %r" % (response,))
+
---
+++ Code/subpub/server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import pika
+import os
+__author__ = 'David'
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+          host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+
+def proof_of_concept(arg):
+    # os.system("gnome-terminal -e 'bash -c \"echo %s; exec bash\"'" % arg)
+    os.system("start /wait powershell /c echo %s" % arg)
+    return arg
+
+
+def on_request(ch, method, props, body):
+    n = str(body)
+    print("Starting terminal printing %s" % (n,))
+    response = proof_of_concept(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
