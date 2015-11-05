dc14ad16b64daefa92f91093cbeba6a4fc44d52d
Carlos <cruiz1391@gmail.com>
2015-11-05 10:03:02 -0500

    server setup files updated

--- Code/tunneler/server/setup.sh
+++ Code/tunneler/server/setup.sh
@@ -3,10 +3,10 @@ SETUP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SETUP_DIR
 cd ..
 cp -rf server_agent /var/opt/
-runOnReboot="sudo update-rc.d /var/opt/Server/on_startup.sh defaults"
-cp -rf /var/opt/server_agent/on_startup.sh /etc/init.d/
+runOnReboot="sudo update-rc.d /var/opt/server/on_startup.sh defaults"
+cp -rf /var/opt/server/on_startup.sh /etc/init.d/
 sudo chmod +x /etc/init.d/on_startup.sh
 sudo update-rc.d on_startup.sh defaults
 sudo update-rc.d on_startup.sh start defaults
 sudo update-rc.d on_startup.sh stop defaults
-python /var/opt/server_agent/Main.py
+python /var/opt/server/Main.py
